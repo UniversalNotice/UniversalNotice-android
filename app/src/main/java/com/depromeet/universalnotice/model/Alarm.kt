6516e1263d4205687a2bafea73240ecdc3fd8e5a
최준영 <chad1994@skuniv.ac.kr>
2019-01-03 17:44:17 +0900

    #5 알림 리스트 임시 구현

--- app/src/main/java/com/depromeet/universalnotice/model/Alarm.kt
+++ app/src/main/java/com/depromeet/universalnotice/model/Alarm.kt
@@ -4,6 +4,7 @@ data class Alarm(
     val category: Int, // 알람 종류 (이미지)
     val title: String, // 이름
     val time: String, // 시간
+    val ampm: String,
     val isAlarm : Boolean, // 알림인지 푸시인지
     val time_interval_type : String, // 이전, 이후
     val time_interval: String, // 간격
--- app/src/main/java/com/depromeet/universalnotice/ui/mainlist/MainListFragment.kt
+++ app/src/main/java/com/depromeet/universalnotice/ui/mainlist/MainListFragment.kt
@@ -5,16 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.depromeet.universalnotice.databinding.FragmentMainlistBinding
-import org.koin.android.ext.android.inject
+import com.depromeet.universalnotice.model.Alarm
 
 class MainListFragment : Fragment() {
-    private val viewModel by inject<MainListViewModel>()
+    private lateinit var viewModel : MainListViewModel
     private lateinit var binding: FragmentMainlistBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentMainlistBinding.inflate(inflater, container, false)
+        setupBinding(savedInstanceState)
 
         return binding.root
     }
@@ -22,16 +24,32 @@ class MainListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val rvLayoutManager =  LinearLayoutManager(requireContext())
-
-        binding.mainlistRecyclerview.apply {
-//            layoutManager = rvLayoutManager
-//            adapter =
-
-        }
-
+//        val rvLayoutManager = LinearLayoutManager(requireContext())
+//
+//        binding.mainlistRecyclerview.apply {
+//            //            layoutManager = rvLayoutManager
+////            adapter =
+//        }
     }
 
+    fun setupBinding(savedInstanceState: Bundle?){
+//        binding = FragmentMainlistBinding.inflate(inflater, container, false)
+        //
+        viewModel = ViewModelProviders.of(this).get(MainListViewModel::class.java)
+        if (savedInstanceState == null) {
+            viewModel.init()
+        }
+        binding.vm = viewModel
 
+        setupList()
+    }
+
+    fun setupList(){
+        viewModel.fetchList()
+        viewModel.getlist().observe(this, Observer<List<Alarm>> {
+            if(it.isNotEmpty())
+                viewModel.setListInAdapter(it)
+        })
+    }
 
 }
